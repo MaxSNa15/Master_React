f5d687448cc7d2255b1a77b3835cfaf7c502fc99
Max Nava <max.s015@outlook.com>
2023-12-12 16:01:06 -0600

    Update CSS and RouterPrincipal
    This commit updates the CSS file by adding a new style for the .activadoss class. It also updates the RouterPrincipal component by replacing the Link component with NavLink and adding a header, navigation menu, and footer.

--- src/routers/RouterPrincipal.tsx
+++ src/routers/RouterPrincipal.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { Routes, Route, Link, BrowserRouter} from 'react-router-dom';
+import { Routes, Route, NavLink, BrowserRouter} from 'react-router-dom';
 import { Paginacion1 } from '../components/Pages/Paginacion1';
 import { Paginacion2 } from '../components/Pages/Paginacion2';
 import { Paginacion3 } from '../components/Pages/Paginacion3';
@@ -7,24 +6,46 @@ import { Paginacion3 } from '../components/Pages/Paginacion3';
 export const RouterPrincipal = () => {
     return (
         <BrowserRouter>
-            <Routes>
-                <Route path='/' element={<Paginacion1/>} />
-                <Route path='/UNO' element={<Paginacion1/>} />
-                <Route path='/DOS' element={<Paginacion2/>} />
-                <Route path='/TRES' element={<Paginacion3/>} />
-                {/* Pagina por defecto */}
-                <Route path='*' element={(
-                    <div>
-                        <h3> Pagina por defecto </h3>
-                        <p> Esta es una pagina de prueba </p>
-                        <p> Numero CUALQUIERA </p>
-                        <Link to='/'> Pagina 1 </Link> <br/>
-                        <Link to='/UNO'> Pagina 1 </Link> <br/>
-                        <Link to='/DOS'> Pagina 2 </Link> <br/>
-                        <Link to='/TRES'> Pagina 3 </Link> <br/>
-                    </div>
-                )} />
-            </Routes>
+            <h1> Cabezera inicial </h1>
+            <h2> Menu de navegacion </h2>
+            {/* Link: para navegar entre paginas */}
+            {/* NavLink: para navegar entre paginas y darle estilos */}
+            <nav>
+                <ul>
+                    <li>
+                        <NavLink className={ ({isActive}) => isActive ? "activadoss" : "" } to='/'>Pagina 1</NavLink>
+                    </li>
+                    <li>
+                        <NavLink className={ ({isActive}) => isActive ? "activadoss" : "" } to='/DOS'>Pagina 2</NavLink>
+                    </li>
+                    <li>
+                        <NavLink className={ ({isActive}) => isActive ? "activadoss" : "" } to='/TRES'>Pagina 3</NavLink>
+                    </li>
+                </ul>
+            </nav>
+            <hr />
+
+            <section className='contenido principal'>
+                {/* Cargar componentes */}
+                {/* Aqui se cargara lso componentes que coicidan con el path */}
+                <Routes>
+                    <Route path='/' element={<Paginacion1/>} />
+                    <Route path='/UNO' element={<Paginacion1/>} />
+                    <Route path='/DOS' element={<Paginacion2/>} />
+                    <Route path='/TRES' element={<Paginacion3/>} />
+                    {/* Pagina por defecto o si hay un error*/}
+                    <Route path='*' element={(
+                        <div>
+                            <h1>Error 404</h1>
+                        </div>
+                    )} />
+                </Routes>
+            </section>
+
+            <hr />
+            <footer>
+                <h2> Pie de pagina </h2>
+            </footer>
         </BrowserRouter>
     )
 }
